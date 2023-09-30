6a083dd9e831ef5decf7186628e220457c80bf5f
Piotr Sajan <piotrek19897@gmail.com>
2023-09-30 21:10:27 +0100

    Zadanie domowe dzien 4 - formatowanie

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -4,25 +4,19 @@ var name = "Ewa";
 var sex = "Kobieta";
 var age = 33;
 
-if
-    (age < 30 && sex == "Kobieta")
+if (age < 30 && sex == "Kobieta")
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if
-
-    (name == "Ewa" && age == 33)
+else if (name == "Ewa" && age == 33)
 {
     Console.WriteLine("Ewa lat 33");
 }
-else if
-    (sex != "Kobieta" && age < 18)
+else if (sex != "Kobieta" && age < 18)
 {
     Console.WriteLine("Niepełnoletni Mężczyzna");
 }
-else if
-
-(sex != "Kobieta" && age >= 18)
+else if (sex != "Kobieta" && age >= 18)
 {
     Console.WriteLine("Pełnoletni mężczyzna");
 }
