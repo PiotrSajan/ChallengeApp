63f9f578e72532d0960f3a004b2475d50df1ad93
Piotr Sajan <piotrek19897@gmail.com>
2023-10-25 22:29:22 +0100

    Zadanie domowe dzien 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,74 +1,48 @@
-﻿// Zadanie domowe dzien 5 
+﻿
+// Zadanie domowe dzien 6 
 
-int number = 45674;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+using ChallengeApp;
 
-foreach (char letter in letters)
+Employee employee1 = new Employee("Adam", "Małysz", 88);
+Employee employee2 = new Employee("Joanna", "Wielka", 22);
+Employee employee3 = new Employee("Krzysztof", "Jeż", 18);
+
+employee1.AddScore(10);
+employee1.AddScore(10);
+employee1.AddScore(10);
+employee1.AddScore(10);
+employee1.AddScore(10);
+
+employee2.AddScore(9);
+employee2.AddScore(10);
+employee2.AddScore(4);
+employee2.AddScore(2);
+employee2.AddScore(2);
+
+employee3.AddScore(8);
+employee3.AddScore(4);
+employee3.AddScore(2);
+employee3.AddScore(2);
+employee3.AddScore(9);
+
+List<Employee> employees = new List<Employee>()
 {
-    if (letter == '0')
-    {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
-    }
+    employee1, employee2, employee3
+};
 
-}
-Console.WriteLine("Ile razy liczby pojawiaja sie w podanym przykładzie?");
-Console.WriteLine("Przykladowa liczba to:" + number);
-Console.WriteLine("Wynik:");
-Console.WriteLine("0=" + counter0);
-Console.WriteLine("1=" + counter1);
-Console.WriteLine("2=" + counter2);
-Console.WriteLine("3=" + counter3);
-Console.WriteLine("4=" + counter4);
-Console.WriteLine("5=" + counter5);
-Console.WriteLine("6=" + counter6);
-Console.WriteLine("7=" + counter7);
-Console.WriteLine("8=" + counter8);
-Console.WriteLine("9=" + counter9);
+int maxResult = -1;
+Employee employeeWithMaxResults = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
+    {
+
+        employeeWithMaxResults = employee;
+        maxResult = employee.Result;
+    }
+};
+
+Console.WriteLine("Pracownik z największą ilościa punktów to:" + " " + employeeWithMaxResults.Name + " " + employeeWithMaxResults.Surname + " " + "lat:" + " " + employeeWithMaxResults.Age);
+Console.WriteLine("Z wynikiem: " + employeeWithMaxResults.Result + " " + "punktów.");
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+List string[] dayOfWeeks = new List<string>();
+
+dayOfWeeks[0] = "poniedziałek";
+dayOfWeeks[1] = "wtorek";
+dayOfWeeks[2] = "sroda";
+dayOfWeeks[3] = "czwartek";
+dayOfWeeks[4] = "piątek";
+dayOfWeeks[5] = "sobota";
+dayOfWeeks[6] = "niedziela";
+
+
+for(int i=0; i<dayOfWeeks.Count; i++)
+
+{
+    Console.WriteLine(dayOfWeeks[i]);
+}
+
