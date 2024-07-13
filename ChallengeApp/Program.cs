f8909bcd14326b8fec92e5f2123e3fa14f4a6258
Piotr Sajan <piotrek19897@gmail.com>
2024-07-13 18:55:11 +0100

    Zadanie domowe dzień 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Zadanie domowe dzien 4 
+var name = "Karolina";
+var sex = "female";
+var age = 27;
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30.");
+
+}
+else if (!(sex == "female") && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna.");
+}
